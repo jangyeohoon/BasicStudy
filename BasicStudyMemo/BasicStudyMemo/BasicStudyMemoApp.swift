5efd601e5fe4168771b766678299f5b813a4f5ce
jangyeohoon <someday0307@naver.com>
2023-07-12 22:15:33 +0900

    0712 BasicStudyMemoApp

---
+++ BasicStudyMemo/BasicStudyMemo/BasicStudyMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicStudyMemoApp.swift
+//  BasicStudyMemo
+//
+//  Created by 장여훈 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct BasicStudyMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
